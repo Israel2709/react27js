7fe5cf39b4e4f5b503c31b509f249928e7239b3e
Israel Salinas <israel.salinas.m@gmail.com>
2023-09-04 19:16:52 -0600

    blank

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,8 @@
 import "./App.css";
-import MentorCard from "./Components/MentorCard";
-import CardWrapper from "./Components/CardWrapper";
 
 function App() {
   return (
-    <>
-      <MentorCard
-        name="Israel"
-        description="Hola, soy mentor front-end de kodemia"
-      />
-      <MentorCard
-        name="Charles"
-        description="Hola, soy mentor back-end de kodemia"
-      />
-
-      <CardWrapper>
-        <img src="" alt="" />
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat,
-          similique!
-        </p>
-        <h1>Lorem ipsum dolor sit amet.</h1>
-        <input type="text" />
-        <input type="text" />
-        <input type="text" />
-      </CardWrapper>
-
-      <CardWrapper>
-        <h1>Contenido 1</h1>
-      </CardWrapper>
-
-      <CardWrapper>
-        <ul>
-          <li>item 01</li>
-          <li>item 02</li>
-          <li>item 03</li>
-          <li>item 04</li>
-          <li>item 05</li>
-        </ul>
-      </CardWrapper>
-    </>
+    
   );
 }
 
